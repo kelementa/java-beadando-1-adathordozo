3585c1a2538f7fcb0c56d97fec3a2e39b6709692
kelementa <kelementa@gmail.com>
2018-09-24 23:07:03 +0200

    Feladat class szerkesztése

---
+++ feladat/src/feladat/Feladat.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feladat;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kelemen Tamás 1/14ED
+ */
+public class Feladat {
+    
+    static ArrayList<Adathordozo> adathordozo = new ArrayList<>();
+    static Adathordozo adathordozo2[] = new Adathordozo[4];
+    
+    static void feltoltes() {
+        // tömb feltöltése
+        adathordozo.add(new Merevlemez(1000, 5400));
+        adathordozo.add(new Merevlemez(500, 7200));
+        adathordozo.add(new Pendrive(32, 2));
+        adathordozo.add(new Pendrive(128, 3));
+        
+        adathordozo2[0] = new Merevlemez(1000, 5400);
+        adathordozo2[1] = new Merevlemez(500, 7200);
+        adathordozo2[2] = new Pendrive(32, 2);
+        adathordozo2[3] = new Pendrive(128, 3);
+    }
+    
+    static void listazas() {
+        // tömb tartalmának kirása
+        for (int i = 0; i < adathordozo.size(); i++) {
+            System.out.println(adathordozo.get(i).toString());
+        }
+        
+        for (int i = 0; i < adathordozo2.length; i++) {
+            System.out.println(adathordozo2[i].toString());
+        }
+    }
+    
+    
+    static void legnagyobb() {
+        // a legnagyobb adathordozó meghatározása
+        int legnagyobb = 0;
+        for (int i = 0; i < adathordozo.size(); i++) {
+            if (adathordozo.get(i).getKapacitas() > legnagyobb) {
+                System.out.println(adathordozo.get(i).getKapacitas());
+                legnagyobb = i;
+                System.out.println(legnagyobb);
+            }
+        }
+        System.out.println("A legnagyobb: " + adathordozo.get(legnagyobb).toString()); 
+        
+        int max = 0;
+        for (int i = 0; i < adathordozo2.length; i++) {
+            if (adathordozo2[i].getKapacitas() > max) {
+                System.out.println(adathordozo2[i].getKapacitas());
+                max = i;
+                System.out.println(max);
+            }
+        }
+        System.out.println("A legnagyobb: " + adathordozo2[max].toString());
+    }
+    
+    static void csakPendrive() {
+        // csak a pendrive-ok listázása
+        System.out.println("Pendrive-ok: ");
+        for (int i = 0; i < adathordozo.size(); i++) {
+            if (adathordozo.get(i) instanceof Pendrive) {
+                System.out.println(adathordozo.get(i).toString());
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        feltoltes();
+        listazas();
+        legnagyobb();
+        csakPendrive();
+        
+        
+    }
+    
+}
